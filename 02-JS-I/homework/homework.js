d80032c5f6c3c39c1c27c46ba3f7c500458fb639
syoshi20 <sebastianyoshitsuru@gmail.com>
2022-01-12 18:11:40 -0300

    tarea1 v.3

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,9 +60,9 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (x===y) 
-    {return true;}
-      eslse   {return false;}
+  if (x===y) {
+    return true;
+  } else {return false;}
 }
 
 function tienenMismaLongitud(str1, str2) {
