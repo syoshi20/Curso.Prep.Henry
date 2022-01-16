4dab30f15710750555594b40e25888699e20634c
syoshi20 <sebastianyoshitsuru@gmail.com>
2022-01-15 22:51:20 -0300

    tarea5 v.6

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -84,7 +84,7 @@ function tienePropiedad(objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (objeto.propiedad) 
+  if (objeto[propiedad]) 
   {
     return true;
   }else{return false;}
